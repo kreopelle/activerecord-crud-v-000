c699c16e327615d11e7c1e88ee5c0c373ad3e7cf
Kayla Reopelle <kreopelle@gmail.com>
2018-07-02 11:41:05 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,13 +22,20 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {title: "Home Alone", release_date: "1990"})
+# def can_be_created_in_a_block(args = {title: "Home Alone", release_date: "1990"})
+#   Movie.create do |m|
+#     m.title = args[:title]
+#     m.release_date = args[:release_date]
+#     m.director = args[:director]
+#     m.lead = args[:lead]
+#     m.in_theaters = args[:in_theaters]
+#   end
+# end
+
+
+def can_be_created_in_a_block(args = {})
   Movie.create do |m|
-    m.title = args[:title]
-    m.release_date = args[:release_date]
-    m.director = args[:director]
-    m.lead = args[:lead]
-    m.in_theaters = args[:in_theaters]
+    args.each {|key, value| self.send(("#{key}="), value)}
   end
 end
 
